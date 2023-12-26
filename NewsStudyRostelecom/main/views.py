34838df95c315047caa927e7d4e93ede4bbba8f7
iphyperio <iphyperio@gmail.com>
2023-12-26 14:14:21 +0700

    26.12 Подключаем автообновление

--- NewsStudyRostelecom/main/views.py
+++ NewsStudyRostelecom/main/views.py
@@ -190,10 +190,35 @@ def selectlanguage(request):
 #     else:
 #         return HttpResponse('some kind of error')
 
+import hashlib
+import hmac
+from django.http import HttpResponseServerError
+def verify_signature(payload_body, secret_token, signature_header):
+    """Verify that the payload was sent from GitHub by validating SHA256.
+
+    Raise and return 403 if not authorized.
+
+    Args:
+        payload_body: original request body to verify (request.body())
+        secret_token: GitHub app webhook token (WEBHOOK_SECRET)
+        signature_header: header received from GitHub (x-hub-signature-256)
+    """
+    if not signature_header:
+        return HttpResponseServerError("x-hub-signature-256 header is missing!", status_code=403)
+    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+    expected_signature = "sha256=" + hash_object.hexdigest()
+    if not hmac.compare_digest(expected_signature, signature_header):
+        return HttpResponseServerError("Request signatures didn't match!", status_code=403)
+
+
 import git
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
 @csrf_exempt
 def update_server(request):
+    header_signature = request.META.get('HTTP_X_HUB_SIGNATURE')
+    verify_signature(request.body,settings.GITHUB_WEBHOOK_KEY,header_signature)
+
     if request.method == "POST":
         local_dir = '/home/demouserrostelecom/DjangoNewsRostelecom'
         repo = git.Repo(local_dir)
